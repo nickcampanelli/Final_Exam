32eae7c4bbeed090c81acb456f9b27da238dec6a
Nick <Nick@128.4.139.250>
2017-05-21 22:54:06 -0400

    First Commit

--- RocketBLL/src/main/java/rocketBase/RateBLL.java
+++ RocketBLL/src/main/java/rocketBase/RateBLL.java
@@ -6,50 +6,32 @@ import org.apache.poi.ss.formula.functions.*;
 
 import exceptions.RateException;
 import rocketDomain.RateDomainModel;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class RateBLL {
 
 	private static RateDAL _RateDAL = new RateDAL();
 	
-	static double getRate(int GivenCreditScore) throws RateException 
+	public static double getRate(int GivenCreditScore) throws RateException 
 	{
 		double dInterestRate = 0;
-		
-		//TODO - RocketBLL RateBLL.getRate - make sure you throw any exception
-		
-		//		Call RateDAL.getAllRates... this returns an array of rates
-		//		write the code that will search the rates to determine the 
-		//		interest rate for the given credit score
-		//		hints:  you have to sort the rates...  you can do this by using
-		//			a comparator... or by using an OrderBy statement in the HQL
-		
-		
-		//TODO - RocketBLL RateBLL.getRate
-		//			obviously this should be changed to return the determined rate
-		
-		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
-		
 
-		//TODO: Filter the ArrayList...  look for the correct rate for the given credit score.
-		//	Easiest way is to apply a filter using a Lambda function.
-		//
-		//	Example... how to use Lambda functions:
-		//			https://github.com/CISC181/Lambda
+		ArrayList<RateDomainModel> Rates = (ArrayList<RateDomainModel>) RateDAL.getAllRates().stream().filter(r -> r.getiMinCreditScore()<=GivenCreditScore).collect(Collectors.toList());
+
+		if(Rates.isEmpty()){
+			RateDomainModel rde = new RateDomainModel();
+			rde.setiMinCreditScore(GivenCreditScore);
+			throw new RateException(rde);
+		}
+		else{
+			dInterestRate = Rates.get(0).getdInterestRate();
+		}
 		
 		return dInterestRate;
 		
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	//TODO - RocketBLL RateBLL.getPayment 
-	//		how to use:
-	//		https://poi.apache.org/apidocs/org/apache/poi/ss/formula/functions/FinanceLib.html
 	
 	public static double getPayment(double r, double n, double p, double f, boolean t)
 	{		
--- RocketServer/src/main/java/rocketServer/RocketHub.java
+++ RocketServer/src/main/java/rocketServer/RocketHub.java
@@ -2,6 +2,7 @@ package rocketServer;
 
 import java.io.IOException;
 
+import exceptions.RateException;
 import netgame.common.Hub;
 import rocketBase.RateBLL;
 import rocketData.LoanRequest;
@@ -23,16 +24,15 @@ public class RocketHub extends Hub {
 			resetOutput();
 			
 			LoanRequest lq = (LoanRequest) message;
-			
-			//	TODO - RocketHub.messageReceived
 
-			//	You will have to:
-			//	Determine the rate with the given credit score (call RateBLL.getRate)
-			//		If exception, show error message, stop processing
-			//		If no exception, continue
-			//	Determine if payment, call RateBLL.getPayment
-			//	
-			//	you should update lq, and then send lq back to the caller(s)
+			try{
+				lq.setdRate(RateBLL.getRate(lq.getiCreditScore())/1200);
+			}
+			catch(RateException re){
+				re.printStackTrace();
+			}
+			
+			lq.setdPayment(RateBLL.getPayment(lq.getdRate(), lq.getiTerm(), lq.getdAmount()-lq.getiDownPayment(), 0, false)*-1);
 			
 			sendToAll(lq);
 		}
