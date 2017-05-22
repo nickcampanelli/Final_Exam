b57b77a021e2f8dbbc3bd9cb9dd11a97f839bb0b
Nick <Nick@128.4.139.250>
2017-05-21 23:42:26 -0400

    Final Commit

--- RocketBLL/src/test/java/rocketBase/rate_test.java
+++ RocketBLL/src/test/java/rocketBase/rate_test.java
@@ -2,17 +2,28 @@ package rocketBase;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import exceptions.RateException;
+import rocketDomain.RateDomainModel;
 
 public class rate_test {
 
-	//TODO - RocketBLL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketBLL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
+	@Test
+	public void rate_test1(){
+		ArrayList<RateDomainModel> test1 = RateDAL.getAllRates(); 
+		assertTrue(test1.get(1).getiMinCreditScore()==600);
+		test1.get(1).setiMinCreditScore(600);
+		assertTrue(test1.get(1).getdInterestRate()==7);
+	}
+
+	@Test(expected=RateException.class)
+	public void rate_test2() throws RateException{
+		RateDomainModel test1 = new RateDomainModel(); 
+		test1.setiMinCreditScore(0);
+
+	}
 
 }
--- RocketDAL/src/test/java/rocketBase/Rate_Test.java
+++ RocketDAL/src/test/java/rocketBase/Rate_Test.java
@@ -10,13 +10,18 @@ import rocketDomain.RateDomainModel;
 
 public class Rate_Test {
 
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
+	@Test
+	public void rate_test1(){
+		ArrayList<RateDomainModel> test1 = RateDAL.getAllRates(); 
+		assertTrue(test1.get(1).getiMinCreditScore()==600);
+		test1.get(1).setiMinCreditScore(600);
+		assertTrue(test1.get(1).getdInterestRate()==7);
+		
+	}
 
+	@Test
+	public void rate_test2(){
+		
+	}
 
 }
